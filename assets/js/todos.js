0bac5d8102481cc937953aef6cbf1225be09a6bd
EugeneKovalov <eugene.kovalev@me.com>
2017-11-05 21:17:19 +0200

    raw toggling list items

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,13 @@
-alert('Connected');
+$("li").click(function() {
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    } else {
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+});
