5aa7f196b12f420e3c3dda7b87316be189b2d589
EugeneKovalov <eugene.kovalev@me.com>
2017-11-05 22:21:48 +0200

    removing items with fadeOut animations

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,10 @@
 $("li").click(function() {
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
-    } else {
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
+    $(this).toggleClass("completed");
+});
+
+$("span").click(function(e) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
